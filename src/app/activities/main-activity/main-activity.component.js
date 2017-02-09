0fb20d5120defbfdc02cc8af4a11085a10561c2a
sgolikov <sgolikov.im@gmail.com>
2017-02-09 12:44:23 +0300

    Add main activity

--- src/app/activities/main-activity/main-activity.component.js
+++ src/app/activities/main-activity/main-activity.component.js
@@ -1,17 +1,94 @@
 const MODULE_NAME = 'gt.main.activity';
 
+import './main-activity.component.scss';
+
+import gtProjectInfo from '../../components/project-info/project-info.component.js';
+import gtProjectSelector from '../../components/project-selector/project-selector.component.js';
+import gtUserSearch from '../../components/user-search/user-search.component.js';
+import gtUserSelector from '../../components/user-selector/user-selector.component.js';
+import gtApiGitHub from '../../api/github.service.js';
+
 let component = {
     template: require('./main-activity.component.pug'),
-    bindings: {
-    },
-    controller: function() {
+    controller: ['GitHubProject', 'GitHubUser', function(GitHubProject, GitHubUser) {
         var vm = this;
 
-        vm.data = 'Main Activity!';
-    }
+        vm.cleanUsers = function() {
+            vm.users = [];
+            vm.activeUser = undefined;
+            vm.loadProjects();
+        }
+
+        vm.cleanProjects = function() {
+            vm.projects = [];
+            vm.activeProject = undefined;
+        }
+
+        vm.onUserSearch = function(search) {
+            vm.loadUsers(search);
+        }
+
+        vm.onUserSelect = function(user) {
+            vm.activeUser = user;
+            vm.loadProjects();
+        }
+
+        vm.onProjectSelect = function(project) {
+            vm.activeProject = project;
+        }
+
+        vm.loadUsers = function(search) {
+            vm.error = undefined;
+
+            if (!search) {
+                vm.cleanUsers();
+            } else {
+                GitHubUser.get({ q: search }, function(users) {
+                    vm.users = users.items;
+                    if (vm.users.length) {
+                        vm.activeUser = vm.users[0];
+                        vm.loadProjects();
+                    } else {
+                        vm.cleanUsers();
+                    }
+                }, function(error) {
+                    vm.cleanUsers();
+                    vm.error = error.data.message;
+                });
+            }
+        }
+
+        vm.loadProjects = function() {
+            vm.error = undefined;
+
+            if (!vm.activeUser) {
+                vm.cleanProjects();
+            } else {
+                GitHubProject.query({ username: vm.activeUser.login }, function(projects) {
+                    vm.projects = projects;
+                    if (vm.projects.length) {
+                        vm.activeProject = vm.projects[0];
+                    } else {
+                        vm.cleanProjects();
+                    }
+                }, function(error) {
+                    vm.cleanUsers();
+                    vm.error = error.data.message;
+                });
+            }
+        }
+    }]
 };
 
-angular.module(MODULE_NAME, [])
-  .component('gtMainActivity', component);
+angular.module(MODULE_NAME, [
+    gtProjectInfo,
+    gtProjectSelector,
+    gtUserSearch,
+    gtUserSelector,
+    gtApiGitHub
+])
+
+angular.module(MODULE_NAME)
+    .component('gtMainActivity', component);
 
 export default MODULE_NAME;
--- src/app/app.js
+++ src/app/app.js
@@ -1,29 +1,12 @@
 import angular from 'angular';
 
+import './app.scss';
 import gtMainActivity from './activities/main-activity/main-activity.component.js';
-
-let app = () => {
-  return {
-    template: '<gt-main-activity></gt-main-activity>',
-    controller: 'AppCtrl',
-    controllerAs: 'app'
-  }
-};
-
-class AppCtrl {
-  constructor() {
-    this.url = 'https://github.com/preboot/angular-webpack';
-  }
-}
 
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [
     gtMainActivity
 ]);
-
-angular.module(MODULE_NAME)
-  .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
 
 export default MODULE_NAME;
